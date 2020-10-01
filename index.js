89573567891afeba3e57ded581f6e6d1ab6aa95d
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-01 11:07:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
+  updateDom();
+});
+
+function updateDom() {
   let element = document.getElementById('text');
   element.innerHTML = "This is really cool!";
-});
+}
