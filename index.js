1fc8af57ca22c56b496e150a90ceb96e62395ea7
Ethan <eleetyson@gmail.com>
2020-10-07 19:14:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// addEventListener takes a string with the name of the event...
+// and a callback function
+document.addEventListener( "DOMContentLoaded", function() {
+  replaceText();
+} );
+
+// callback function changes text of the p element with id="text"
+function replaceText() {
+  document.querySelector("p#text").innerHTML = "This is really cool!";
+}
